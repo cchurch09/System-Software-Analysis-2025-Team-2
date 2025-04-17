e216ec4b1cac3247b3697d54cceb41273314a3cf
Darcy Ralstin <fayeralstin@gmail.com>
2025-04-17 06:21:08 -0400

    changed buttons from "window size" placeholder to "rotate paddles"

--- custom.py
+++ custom.py
@@ -6,5 +6,7 @@ class customMenu(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Customization Menu")
-        windowButton = tk.Button(self, "Change window size")
-        windowButton.pack()
+        rotatePaddlesOn = tk.Button(self, text="Turn on rotating paddles")
+        rotatePaddlesOff = tk.Button(self, text="Turn off rotating paddles")
+        rotatePaddlesOn.pack()
+        rotatePaddlesOff.pack()
